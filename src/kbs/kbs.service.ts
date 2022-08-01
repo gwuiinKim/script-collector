566758066b8994a66692568d72fee395bc6bd18c
gwuiinkim <gwuiinkim@gmail.com>
2022-08-02 04:24:12 +0900

    version1 finished.

--- src/kbs/kbs.service.ts
+++ src/kbs/kbs.service.ts
@@ -11,6 +11,7 @@ const url =(date:string) => `https://news.kbs.co.kr/vod/program.do?bcd=0001#${da
 @Injectable()
 export class KbsService {
     async getByDate(date:string):Promise<string[]> {
+        const texts = [];
 
         try{
                 const browser = await puppeteer.launch({headless:false});
@@ -18,8 +19,6 @@ export class KbsService {
                 await page.goto(url("20220801"));
                 await page.waitForSelector("ul#thumbnailNewsList");
                 
-
-               
                 const addresses = await page.evaluate(()=> {
                     const a = document.querySelectorAll("ul#thumbnailNewsList > li > a");
                     const temp = [];
@@ -30,13 +29,40 @@ export class KbsService {
                     return temp;
                 })
 
-               console.log(addresses);
-              
-            
-            
+                console.log(addresses);
+
+                
+                for (let i=0; i<addresses.length; i++) {
+                    await page.goto(addresses[i]);
+                    await page.waitForSelector("div#cont_newstext");
+                    const text = await page.evaluate(()=> {
+                            const div = document.querySelector("div#cont_newstext");
+                            const child = div.getElementsByTagName("div");
+                            if (child && child.length>0) {
+                                // 이렇게 하면 앵커만 나옴
+                                let text= child[0].innerText;
+                                console.log(text);
+                                return text;
+                            } else {
+                                let text = div.innerHTML;
+                                text = text.replace("<!-- 서비스 기사 -->", "");
+                                text = text.replace("<!-- 기사본문 필요한 부분을 치환 -->", "");
+                                text = text.replace(/<b>/g, "");
+                                text = text.replace(/<\/b>/g, "");
+                                const s= text.split("<br>");
+                                const f = s.join("\n");
+                                console.log(text);
+                                return f;
+                            }
+                        })
+                   texts.push(text);
+                }
+                
+
+               
         }catch(e) {
         }
 
-        return ["hi"];
+        return texts;
     }
 }
